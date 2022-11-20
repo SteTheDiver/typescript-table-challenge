a327b7f721500ab33fafb41a287fb9724623d6ae
SteTheDiver <stefano.cattaneo86@gmail.com>
2022-11-20 22:06:33 +0100

    Modified checkbox input, and adjusted styling

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { ChangeEvent } from "react";
 
 import styled from "styled-components";
 import { AiOutlineClose } from "react-icons/ai";
@@ -11,6 +11,7 @@ type Props = {
   toggleStatus: (id: number, status: boolean) => void;
   handleName: (id: number, name: string) => void;
   handleEmail: (id: number, email: string) => void;
+  activeUser: string[];
 };
 
 const Modal = ({
@@ -19,8 +20,8 @@ const Modal = ({
   toggleStatus,
   handleName,
   handleEmail,
-}: //   foo
-Props) => {
+  activeUser,
+}: Props) => {
   console.log(allUsers);
 
   return (
@@ -58,7 +59,9 @@ Props) => {
                   name="name"
                   id=""
                   value={name}
-                  onChange={(e: any) => handleName(id, e.target.value)}
+                  onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                    handleName(id, e.target.value)
+                  }
                   style={{ backgroundColor: "transparent", border: "none" }}
                 />
               </td>
@@ -68,20 +71,40 @@ Props) => {
                   name="email"
                   id=""
                   value={email}
-                  onChange={(e: any) => handleEmail(id, e.target.value)}
+                  onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                    handleEmail(id, e.target.value)
+                  }
                   style={{ backgroundColor: "transparent", border: "none" }}
+                  required
                 />
               </td>
-              <td>
-                <input
-                  type="checkbox"
-                  name="active"
-                  id=""
-                  onChange={(e: any) => toggleStatus(id, e.target.checked)}
-                  checked={active}
-                  // disabled={ ? true : false}
-                />
-                {active ? "Online" : "Offline"}
+              <td style={{ width: "fit-content" }}>
+                {/* <label htmlFor="">
+                  <input
+                    type="checkbox"
+                    name="active"
+                    id=""
+                    onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                      toggleStatus(id, e.target.checked)
+                    }
+                    checked={active}
+                    disabled={!active && activeUser.length >= 1 ? true : false}
+                  />
+                </label> */}
+                <label className="switch">
+                  <input
+                    type="checkbox"
+                    name="active"
+                    id=""
+                    onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                      toggleStatus(id, e.target.checked)
+                    }
+                    checked={active}
+                    disabled={!active && activeUser.length >= 1 ? true : false}
+                  />
+                  <span className="slider round"></span>
+                </label>
+                {/* {active ? "Online" : "Offline"} */}
               </td>
             </tr>
           );
@@ -92,53 +115,146 @@ Props) => {
 };
 
 const Wrapper = styled.section`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
   display: flex;
   flex-direction: column;
-  align-content: center;
-  position: absolute;
-  top: 20%;
-  left: 25%;
-  width: 40%;
+  overflow: auto;
+  width: 90%;
   background-color: rgba(245, 245, 245, 1);
-  height: 50%;
   overflow: auto;
   border: 1px solid lightgray;
   border-radius: 4px;
+  height: 50%;
+
+  @media (min-width: 1000px) {
+    width: 60%;
+  }
 
   header {
-    background-color: rgba(245, 245, 245, 0.5);
+    background-color: #dfd7d7;
     width: 100%;
     border-bottom: 1px solid #3f1111;
     display: flex;
     justify-content: center;
+    height: 3rem;
+    position: sticky;
+    top: 0;
+    z-index: 100;
 
     .header-center {
-      width: 90%;
+      width: 97%;
       display: flex;
       justify-content: space-between;
+      align-items: center;
+      /* height: 50px; */
     }
   }
 
   .btn-close {
-    color: red;
+    color: black;
     background-color: transparent;
     border: none;
-    font-size: 1.5rem;
+    font-size: 1.7rem;
+    cursor: pointer;
+
+    :hover {
+      color: #d80d30;
+    }
   }
 
   table {
-    padding: 20px;
+    /* height: 50%; */
+    padding: 10px;
+  }
+
+  thead {
+    /* height: 2em; */
+    position: sticky;
+    top: 15px;
+    font-size: 20px;
   }
 
   tr {
     display: flex;
     flex-direction: row;
     justify-content: space-between;
+    align-items: center;
+    height: 2rem;
+    width: 100%;
+    margin-top: 4px;
+
+    td {
+      text-align: left;
+      width: 100%;
+      padding-right:10px
+    }
+
+    input {
+      font-size: 16px;
+      width: inherit;
+      max-width: inherit;
+    }
   }
 
-  td {
-    text-align: left;
-    width: fit-content;
+  .switch {
+    position: relative;
+    display: inline-block;
+    width: 50px;
+    height: 22.5px;
+  }
+
+  .switch input {
+    opacity: 0;
+    width: 0;
+    height: 0;
+  }
+
+  .slider {
+    position: absolute;
+    cursor: pointer;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #c71d1d;
+    -webkit-transition: 0.4s;
+    transition: 0.4s;
+  }
+
+  .slider:before {
+    position: absolute;
+    content: "";
+    height: 15px;
+    width: 15px;
+    left: 4px;
+    bottom: 3.9px;
+    background-color: white;
+    -webkit-transition: 0.2s;
+    transition: 0.2s;
+  }
+
+  input:checked + .slider {
+    background-color: #1c8b12;
+  }
+
+  input:focus + .slider {
+    box-shadow: 0 0 1px #2196f3;
+  }
+
+  input:checked + .slider:before {
+    transform: translateX(26px);
+  }
+
+  /* Rounded sliders */
+  .slider.round {
+    border-radius: 34px;
+  }
+
+  .slider.round:before {
+    border-radius: 50%;
   }
 `;
 
