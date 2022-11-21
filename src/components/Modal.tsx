9e7b16aede621ca9b53bbec076233a9d9ddf3dc5
SteTheDiver <stefano.cattaneo86@gmail.com>
2022-11-21 22:05:00 +0100

    Added autoScroll, modified input logic and overall styling

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent } from "react";
+import React, { ChangeEvent, useEffect } from "react";
 
 import styled from "styled-components";
 import { AiOutlineClose } from "react-icons/ai";
@@ -8,10 +8,9 @@ type Props = {
   allUsers: User[];
   setAllUsers: Function;
   toggle: () => void;
-  toggleStatus: (id: number, status: boolean) => void;
+  toggleStatus: (id: number, active: boolean) => void;
   handleName: (id: number, name: string) => void;
   handleEmail: (id: number, email: string) => void;
-  activeUser: string[];
 };
 
 const Modal = ({
@@ -20,9 +19,31 @@ const Modal = ({
   toggleStatus,
   handleName,
   handleEmail,
-  activeUser,
 }: Props) => {
-  console.log(allUsers);
+  const refs = allUsers.reduce((acc: any, value) => {
+    acc[value.id] = React.createRef();
+    return acc;
+  }, {});
+
+  const handleLoad = (id: number) =>
+    refs[id]?.current.scrollIntoView({
+      behavior: "smooth",
+      block: "center",
+    });
+
+  let activeArr: number[] = allUsers
+    .filter((user) => user.active === true)
+    .map((user) => {
+      return user.id;
+    });
+
+  const activeId: number | any = activeArr.pop();
+
+  const handleScroll = () => handleLoad(activeId);
+
+  useEffect(() => {
+    handleScroll();
+  });
 
   return (
     <Wrapper>
@@ -36,79 +57,61 @@ const Modal = ({
       </header>
       <table>
         <thead>
-          <tr>
+          <tr className="row-header">
             <th>Name</th>
             <th>Email</th>
             <th>Status</th>
           </tr>
         </thead>
-        {allUsers.map((user) => {
-          const { id, name, email, active } = user;
-          return (
-            <tr
-              key={id}
-              style={{
-                borderBottom: "1px solid grey",
-                marginTop: "4px",
-                padding: "0,3px",
-              }}
-            >
-              <td>
-                <input
-                  type="text"
-                  name="name"
-                  id=""
-                  value={name}
-                  onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                    handleName(id, e.target.value)
-                  }
-                  style={{ backgroundColor: "transparent", border: "none" }}
-                />
-              </td>
-              <td>
-                <input
-                  type="email"
-                  name="email"
-                  id=""
-                  value={email}
-                  onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                    handleEmail(id, e.target.value)
-                  }
-                  style={{ backgroundColor: "transparent", border: "none" }}
-                  required
-                />
-              </td>
-              <td style={{ width: "fit-content" }}>
-                {/* <label htmlFor="">
+        <tbody>
+          {allUsers.map((user) => {
+            const { id, name, email, active } = user;
+            return (
+              <tr key={id} className="row-body" ref={refs[id]}>
+                <td>
                   <input
-                    type="checkbox"
-                    name="active"
+                    type="text"
+                    name="name"
                     id=""
+                    required
+                    value={name}
                     onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                      toggleStatus(id, e.target.checked)
+                      handleName(id, e.target.value)
                     }
-                    checked={active}
-                    disabled={!active && activeUser.length >= 1 ? true : false}
+                    style={{ backgroundColor: "transparent", border: "none" }}
                   />
-                </label> */}
-                <label className="switch">
+                </td>
+                <td>
                   <input
-                    type="checkbox"
-                    name="active"
+                    type="email"
+                    name="email"
+                    required
                     id=""
+                    value={email}
                     onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                      toggleStatus(id, e.target.checked)
+                      handleEmail(id, e.target.value)
                     }
-                    checked={active}
-                    disabled={!active && activeUser.length >= 1 ? true : false}
+                    style={{ backgroundColor: "transparent", border: "none" }}
                   />
-                  <span className="slider round"></span>
-                </label>
-                {/* {active ? "Online" : "Offline"} */}
-              </td>
-            </tr>
-          );
-        })}
+                </td>
+                <td style={{ width: "fit-content" }}>
+                  <label className="switch">
+                    <input
+                      type="checkbox"
+                      name="active"
+                      id=""
+                      onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                        toggleStatus(id, e.target.checked)
+                      }
+                      checked={active}
+                    />
+                    <span className="slider round"></span>
+                  </label>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
       </table>
     </Wrapper>
   );
@@ -121,18 +124,15 @@ const Wrapper = styled.section`
   transform: translate(-50%, -50%);
   display: flex;
   flex-direction: column;
-  overflow: auto;
-  width: 90%;
+  overflow: hidden;
+  width: 80%;
   background-color: rgba(245, 245, 245, 1);
-  overflow: auto;
   border: 1px solid lightgray;
   border-radius: 4px;
-  height: 50%;
 
   @media (min-width: 1000px) {
     width: 60%;
   }
-
   header {
     background-color: #dfd7d7;
     width: 100%;
@@ -141,77 +141,109 @@ const Wrapper = styled.section`
     justify-content: center;
     height: 3rem;
     position: sticky;
-    top: 0;
-    z-index: 100;
 
     .header-center {
       width: 97%;
       display: flex;
       justify-content: space-between;
       align-items: center;
-      /* height: 50px; */
+      padding: 10px;
+    }
+
+    .btn-close {
+      display: flex;
+      color: black;
+      background-color: transparent;
+      border: none;
+      font-size: 1.7rem;
+      cursor: pointer;
+
+      :hover {
+        color: #d80d30;
+      }
     }
   }
-
-  .btn-close {
-    color: black;
-    background-color: transparent;
-    border: none;
-    font-size: 1.7rem;
-    cursor: pointer;
-
-    :hover {
-      color: #d80d30;
-    }
-  }
-
   table {
-    /* height: 50%; */
-    padding: 10px;
+    overflow: scroll;
+    table-layout: fixed;
+    font-family: Arial, Helvetica, sans-serif;
   }
 
   thead {
-    /* height: 2em; */
-    position: sticky;
-    top: 15px;
+    height: 2em;
     font-size: 20px;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+
+  tbody {
+    display: block;
+    height: 500px;
+    overflow-y: scroll;
+    padding: 0 5px;
   }
 
   tr {
+    align-items: center;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    height: 2rem;
+    margin-top: 4px;
+    width: 100%;
+  }
+
+  .row-header {
     display: flex;
     flex-direction: row;
     justify-content: space-between;
     align-items: center;
     height: 2rem;
-    width: 100%;
+    width: calc(100% - 15px);
     margin-top: 4px;
 
-    td {
-      text-align: left;
-      width: 100%;
-      padding-right:10px
-    }
+    th {
+      font-size: 15px;
 
-    input {
-      font-size: 16px;
-      width: inherit;
-      max-width: inherit;
+      @media (min-width: 700px) {
+        font-size: 17px;
+      }
+    }
+  }
+
+  .row-body {
+    width: 100%;
+    td {
+      width: 100%;
+    }
+  }
+
+  .row-body:nth-child(odd) {
+    background-color: rgba(229, 215, 218, 0.5);
+  }
+
+  input[type="text"],
+  input[type="email"] {
+    width: 100%;
+    font-size: 11.5px;
+    background-color: transparent;
+
+    @media (min-width: 850px) {
+      width: 80%;
+      font-size: 15px;
     }
   }
 
   .switch {
     position: relative;
     display: inline-block;
-    width: 50px;
+    width: 40px;
     height: 22.5px;
   }
-
   .switch input {
     opacity: 0;
     width: 0;
     height: 0;
   }
-
   .slider {
     position: absolute;
     cursor: pointer;
@@ -220,10 +252,8 @@ const Wrapper = styled.section`
     right: 0;
     bottom: 0;
     background-color: #c71d1d;
-    -webkit-transition: 0.4s;
     transition: 0.4s;
   }
-
   .slider:before {
     position: absolute;
     content: "";
@@ -232,27 +262,20 @@ const Wrapper = styled.section`
     left: 4px;
     bottom: 3.9px;
     background-color: white;
-    -webkit-transition: 0.2s;
     transition: 0.2s;
   }
-
   input:checked + .slider {
     background-color: #1c8b12;
   }
-
   input:focus + .slider {
     box-shadow: 0 0 1px #2196f3;
   }
-
   input:checked + .slider:before {
-    transform: translateX(26px);
+    transform: translateX(17px);
   }
-
-  /* Rounded sliders */
   .slider.round {
     border-radius: 34px;
   }
-
   .slider.round:before {
     border-radius: 50%;
   }
